3ace14980dcd6aebff89771330bd592e494f0b84
岩本 <k_iwamoto@stride-web.co.jp>
2023-04-07 17:20:53 +0900

    レスポンシブ対応

--- components/Transition.js
+++ components/Transition.js
@@ -7,14 +7,14 @@ const variants = {
         opacity: 0,
         // y: 40,
         transition: {
-            duration: 0.45
+            duration: 0.2
         }
     },
     in: {
         opacity: 1,
         // y: 0,
         transition: {
-            duration: 0.45,
+            duration: 0.2,
             delay: 0
         }
     }
