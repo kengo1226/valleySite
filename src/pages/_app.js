346a6cfd52bcdb2f7150e5c2d75dae757931d830
岩本 <k_iwamoto@stride-web.co.jp>
2023-04-11 16:48:25 +0900

    修正

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps, router }) {
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
       <Component key={router.asPath} {...pageProps} />
     </AnimatePresence>
   ) 
