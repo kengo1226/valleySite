86233d21a3cdb3d1fcf07c8215652998773d21f8
岩本 <k_iwamoto@stride-web.co.jp>
2023-04-11 14:49:43 +0900

    修正

--- components/Transition.js
+++ components/Transition.js
@@ -5,18 +5,9 @@ import {useRouter} from "next/router";
 const variants = {
     out: {
         opacity: 0,
-        // y: 40,
-        transition: {
-            duration: 0.2
-        }
     },
     in: {
         opacity: 1,
-        // y: 0,
-        transition: {
-            duration: 0.2,
-            delay: 0
-        }
     }
 }
 
