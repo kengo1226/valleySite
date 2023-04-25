712b6d96a90cb3d4909ee30abd1b912b84b0bcd7
岩本 <k_iwamoto@stride-web.co.jp>
2023-04-25 12:01:19 +0900

    FOUCの修正をします

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,24 @@ import '@/styles/globals.css';
 // import '@/styles/transition.css';
 // import Transition from '../../components/Transition';
 import { AnimatePresence } from "framer-motion";
+import {useState, useEffect} from "react";
 
 export default function App({ Component, pageProps, router }) {
+
+  const [show_screen, setShowScreen] = useState(false);
+
+  useEffect(() => {
+    setShowScreen(true)
+  }, [])
+
   return (
-    <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
-      <Component key={router.asPath} {...pageProps} />
-    </AnimatePresence>
+    <>
+    {show_screen ? <>
+      <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
+        <Component key={router.asPath} {...pageProps} />
+      </AnimatePresence>
+      </>
+      : ""}
+    </>
   ) 
 }
