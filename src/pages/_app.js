d723150e65171f17b0147fe2c408bc36b2fa3a9e
岩本 <k_iwamoto@stride-web.co.jp>
2023-04-25 12:15:05 +0900

    修正

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,24 +2,13 @@ import '@/styles/globals.css';
 // import '@/styles/transition.css';
 // import Transition from '../../components/Transition';
 import { AnimatePresence } from "framer-motion";
-import {useState, useEffect} from "react";
+// import {useState, useEffect} from "react";
 
 export default function App({ Component, pageProps, router }) {
 
-  const [show_screen, setShowScreen] = useState(false);
-
-  useEffect(() => {
-    setShowScreen(true)
-  }, [])
-
   return (
-    <>
-    {show_screen ? <>
       <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
         <Component key={router.asPath} {...pageProps} />
       </AnimatePresence>
-      </>
-      : ""}
-    </>
   ) 
 }
