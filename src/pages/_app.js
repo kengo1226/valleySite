7e0f6bb73665aca6f1d6697a7bca3cc950cb8d3d
岩本 <k_iwamoto@stride-web.co.jp>
2023-04-25 13:03:12 +0900

    修正

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import '@/styles/Home.module.css';
 // import '@/styles/transition.css';
 // import Transition from '../../components/Transition';
 import { AnimatePresence } from "framer-motion";
