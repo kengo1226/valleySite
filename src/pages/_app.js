8bfab436dceecf88e1b40e55b2d3a9d91d0d6580
岩本 <k_iwamoto@stride-web.co.jp>
2023-04-26 19:33:01 +0900

    FOUCの修正

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,17 @@ import '@/styles/globals.css';
 // import '@/styles/transition.css';
 // import Transition from '../../components/Transition';
 import { AnimatePresence } from "framer-motion";
+import "@/styles/styles.css";
 // import {useState, useEffect} from "react";
 
 export default function App({ Component, pageProps, router }) {
+
+  if (typeof window !== "undefined") {
+    const removeFouc = (foucElement) => {
+      foucElement.className = foucElement.className.replace('no-fouc', 'fouc');
+    };
+    removeFouc(document.documentElement);
+  } 
 
   return (
       <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className='no-fouc'>
       <Head />
       <body>
         <script>0</script>
