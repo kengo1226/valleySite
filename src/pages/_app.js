acdc4888d446911dd5d9299ab4597830de69f60d
岩本 <k_iwamoto@stride-web.co.jp>
2023-04-28 16:48:44 +0900

    読み込みタイミングの調整

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from "react";
 import '@/styles/globals.css';
 // import '@/styles/transition.css';
 import "@/styles/styles.css";
@@ -6,9 +7,17 @@ import { AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps }) {
 
-  return (
+  const [isRendered, setIsRendered] = useState(false);
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
+
+  return isRendered ? (
       <AnimatePresence mode='wait'>
         <Component {...pageProps} />
       </AnimatePresence>
-  ) 
+  ) : (
+    <></>
+  )
 }
