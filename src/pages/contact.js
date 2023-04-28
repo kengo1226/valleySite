91183be049e1ff244a998159d0878e45b8a7f96a
岩本 <k_iwamoto@stride-web.co.jp>
2023-04-28 13:07:47 +0900

    CSS修正

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -40,6 +40,10 @@ function contact() {
                 <button type='submit'>Submit</button>
             </form>
         </div>
+
+        <div className={styles.map}>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15148.492471505306!2d98.9508078!3d18.3416668!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30dbc55195ab473b%3A0x890e9931c8c66465!2sHappy%20Valley%20Medical%20Cannabis%20Farm%20-%20L%20A%20M%20P%20H%20U%20N!5e0!3m2!1sja!2sjp!4v1682652951669!5m2!1sja!2sjp" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
     </Layout>
   )
 }
