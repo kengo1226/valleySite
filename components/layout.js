52416e2519824e934e162ab7d60fa2e616d9f8fb
岩本 <k_iwamoto@stride-web.co.jp>
2023-05-01 13:26:58 +0900

    修正

--- components/layout.js
+++ components/layout.js
@@ -8,6 +8,7 @@ import { config } from "@fortawesome/fontawesome-svg-core"
 config.autoAddCss = false
 // framer motion
 import { motion } from "framer-motion"
+import { FaMapMarkerAlt, FaPhone, FaEnvelope } from "react-icons/fa";
 
 const name = "Happy Valley";
 export const siteTitle = "Happy Valley Thailand";
@@ -102,24 +103,37 @@ const menuFunction = () => {
             </motion.div>
             <footer className={styles.footer}>
                 <div className={styles.footerWrapper}>
-                    <ul>
-                        <li>
-                            <Link href="/">HOME</Link>
-                        </li>
-                        <li>
-                            <Link href="/about">ABOUT</Link>
-                        </li>
-                        <li>
-                            <Link href="/licenses">LICENSES</Link>
-                        </li>
-                        <li>
-                            <Link href="/tour">TOUR</Link>
-                        </li>
-                    </ul>
                     <div>
                         <Image src="/happyvalley_textonly.png" width={500} height={300}></Image>
+                        <p>Our farm is open to day tours and overnight stays so that you can learn more about the art of growing, refining, and curing premium indoor and outdoor cannabis. We would look forward to showing you the process of how we make some of the purest and most premium cannabis in the world. </p>
                     </div>
-                    
+                    <ul>
+                        <li>Links</li>
+                        {/* <li>
+                            <Link href="/">Home</Link>
+                        </li> */}
+                        <li>
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link href="/licenses">Licenses</Link>
+                        </li>
+                        <li>
+                            <Link href="/future">Future Plan</Link>
+                        </li>
+                        <li>
+                            <Link href="/tour">Tour</Link>
+                        </li>
+                        <li>
+                            <Link href="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>Contacts</li>
+                        <li><FaPhone /> +66 951392616</li>
+                        <li><FaEnvelope /> info@hvlthai.com</li>
+                        <li><FaMapMarkerAlt /> บ้าน ห้วยเดื่อ, Tha Khum Ngoen, Mae Tha District, Lamphun 51170</li>
+                    </ul>
                 </div>
                 <p>© 2023 Happy Valley Lamphun All Rights Reserved.</p>
             </footer>
